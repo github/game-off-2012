f540bad439d5fe98f7cb5047d393b34d13bdc1fa
yon <Lighthawky@gmail.com>
2012-11-02 14:13:03 +0200

    css and js work

--- app.rb
+++ app.rb
@@ -6,26 +6,11 @@ class ReleaseCyclesApp < Sinatra::Base
   register Sinatra::AssetPack
 
   assets {
-    serve '/javascripts',     from: 'assets/javascripts'
-    serve '/stylesheets',    from: 'assets/stylesheets'
-    serve '/images',         from: 'assets/images'
-    #
-    ## The second parameter defines where the compressed version will be served.
-    ## (Note: that parameter is optional, AssetPack will figure it out.)
-    #js :application, '/js/app.js', [
-    #  '/js/vendor/**/*.js',
-    #  '/js/app/**/*.js'
-    #]
-    #
-    #css :application, '/css/application.css', [
-    #  '/css/screen.css'
-    #]
-    #
-    #js_compression  :jsmin      # Optional
-    #css_compression :sass       # Optional
+    js :application, '/js/app.js', ['/js/*.js']
+    css :application, '/css/app.css', ['/css/*.css']
   }
 
   get '/' do
-    "Hello World!"
+    erb :index
   end
 end
