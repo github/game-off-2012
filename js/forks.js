14dfe79aac2605b1ad6ba11de68d9ea188966bcd
Rishabh <rishabh.pugalia@gmail.com>
2012-11-03 10:27:32 +0530

    getting started with forks

--- js/forks.js
+++ js/forks.js
@@ -2,6 +2,9 @@
 
   window.forks = window.forks || {};
 
+  // Array of random forks
+  var forks = [];
+
   var Fork = function() {
     this.x = 0;
     this.y = 0;
@@ -11,20 +14,38 @@
     // Height
     this.h = 0;
   };
-
+  var flag = 0;
   var createRandomForks = function(ctx, count) {
-    var forks = [];
 
-    for (var i = 0; i <= count; i++) {
-      var fork = new Fork();
-      fork.x = i*100;
-      fork.y = i*100;
+    if (forks.length < count) {
+      
+      for (var i = 0; i < count - forks.length; i++) {
+        var fork = new Fork();
 
-      forks.push(fork);
+        if (forks[forks.length-1]) {
+          fork.x = forks[forks.length-1].x;
+          fork.x += 150;
+
+          fork.y = forks[forks.length-1].y;
+        }
+        else {
+          fork.x = i*150;
+          fork.y = 100;
+        }
+
+        forks.push(fork);
+      }
     }
 
-    forks.forEach(function(fork) {
+    // Loop over forks and draw each of them
+    forks.forEach(function(fork, index) {
+      if (fork.x < 0) {
+        forks.splice(index, 1);
+      }
+      fork.x -= 2;
+
       ctx.beginPath();
+      ctx.strokeStyle = 'blue';
       ctx.lineWidth = 5;
       ctx.moveTo(fork.x, 0);
       ctx.lineTo(fork.x, fork.y);
--- js/main.js
+++ js/main.js
@@ -83,12 +83,34 @@
 
   // There will be only 1 Pappu
   var pappu = {
-    x: 10,
+    x: 50,
     y: 10,
-    w: 10,
-    h: 10,
+    w: 50,
+    h: 50,
+
     draw: function(ctx) {
+      ctx.fillStyle = 'red';
       ctx.fillRect(this.x, this.y, this.w, this.h);
+    },
+
+    hasReachedBoundary: function(canvas_width, canvas_height) {
+      var W = canvas_width;
+      var H = canvas_height;
+
+      // Crossed Sides ?
+      // `c` stands for crossed
+
+      var ctop = (this.y < 0);
+      var cbtm = (this.y > H);
+      var cleft = (this.x < 0);
+      var crgt = (this.x > W);
+
+      // return true if crossed any sides
+      if (ctop || cbtm || cleft || crgt) {
+        return true;
+      }
+
+      return false;
     }
   };
 
@@ -101,8 +123,8 @@
     // Draw Forks
     window.forks.createRandomForks(ctx, 6);
 
-    // Game over on reaching ground
-    if (pappu.y >= H) {
+    // Game over on reaching any boundary
+    if (pappu.hasReachedBoundary(W, H)) {
       return;
     }
     
