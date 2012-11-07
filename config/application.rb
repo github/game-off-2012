52411678cd42ff771eee8c3d187afa07b2b045f7
yon <Lighthawky@gmail.com>
2012-11-07 18:13:13 +0200

    start adding sounds

--- config/application.rb
+++ config/application.rb
@@ -64,5 +64,6 @@ module ReleaseCycles
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    config.assets.paths << Rails.root.join("app", "assets", "sounds")
   end
 end
