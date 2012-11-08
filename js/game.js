f58cfc9fc07f9496e6b0b13dbce2bb423d88f65f
Tom Patterson <tom@printecosoftware.com>
2012-11-08 15:21:10 -0600

    adjusting styles and centering loading/title

--- js/game.js
+++ js/game.js
@@ -5,21 +5,21 @@
 */
 
 var gameBoard = {
-	height: 16,			// Height in tiles
-	width: 16,			// Width in tiles
-	tileSize: 32,		// Tile size in px squared
-	actionKey: 32,		// Spacebar is the action key
+  height: 16,     // Height in tiles
+  width: 16,      // Width in tiles
+  tileSize: 32,   // Tile size in px squared
+  actionKey: 32,    // Spacebar is the action key
 
-	getHeight: function () {
-		return this.height * this.tileSize;
-	},
-	
-	getWidth: function () {
-		return this.width * this.tileSize;
-	}
+  getHeight: function () {
+    return this.height * this.tileSize;
+  },
+  
+  getWidth: function () {
+    return this.width * this.tileSize;
+  }
 }
 
 $(document).ready(function () {
-	Crafty.init(gameBoard.getWidth(), gameBoard.getHeight());
-	Crafty.scene("loading");
+  Crafty.init(gameBoard.getWidth(), gameBoard.getHeight());
+  Crafty.scene("loading");
 });
--- js/scenes/loading.js
+++ js/scenes/loading.js
@@ -5,23 +5,27 @@
 */
 
 Crafty.scene("loading", function () {
-	//load takes an array of assets and a callback when complete
-	Crafty.load(["/images/example.png"], function () {
-		// Creates the sprite for the player
-		Crafty.sprite(32, "/images/example.png", {
-			player: [0, 0]
-		});
-		
-		// Simulate a load time for now to make sure this works
-		setTimeout(function () {
-			Crafty.scene("main"); //when everything is loaded, run the main scene
-		}, 500);
-	});
+  //load takes an array of assets and a callback when complete
+  Crafty.load(["/images/example.png"], function () {
+    // Creates the sprite for the player
+    Crafty.sprite(32, "/images/example.png", {
+      player: [0, 0]
+    });
+    
+    // Simulate a load time for now to make sure this works
+    setTimeout(function () {
+      Crafty.scene("main"); //when everything is loaded, run the main scene
+    }, 500);
+  });
 
-	//black background with some loading text
-	Crafty.background("#000");
-	Crafty.e("2D, DOM, Text").attr({ w: 100, h: 20, x: 150, y: 120 })
-			.text("Loading")
-			.css({  "text-align": "center",
-					"color": "white" });
+  //black background with some loading text
+  Crafty.background("#000");
+  Crafty.e("2D, DOM, Text").attr({ w: 515, h: 515, x: 0, y: 0 })
+      .text("Loading...")
+      .css({  "padding-top": "200px",
+              "text-align": "center",
+              "background-color": "black",
+              "font-weight": "bold",
+              "font-size": "24px",
+              "color": "white" });
 });
