05d3173b10dacf7e799245ab3468f15374dc6f9b
conzett <conzett@gmail.com>
2012-11-18 02:30:00 -0500

    Add animate function
    - Add new placeholder sprite
    - Add different data structures to support animate

--- views/SongView.js
+++ views/SongView.js
@@ -8,10 +8,17 @@ SongView = Backbone.View.extend({
     this.canvas.width  = 640;
     this.canvas.height = 480;
     this.paused = true;
+    this.sprites = new Array();
+    this.sprites['marker'] = new Image();
+    this.sprites['marker'].src = 'img/marker.png';
+    this.queues = _.clone(this.model.get('queues'));
+    this.active   = [[],[],[],[]];
+    this.inactive = [[],[],[],[]];
+    this.missed   = [[],[],[],[]];
     this.context = this.canvas.getContext('2d');
-    _.bindAll(this, 'handleKey');
+    _.bindAll(this, 'handleKey', 'animate');
     $(document).bind('keydown', this.handleKey);
-    console.log(this.model);
+    this.animate();
   },
 
   render: function () { 
@@ -31,6 +38,50 @@ SongView = Backbone.View.extend({
     }else{
 
     }
+  },
+
+  clear: function () {
+    this.context.clearRect(0, 0,
+      this.canvas.width, this.canvas.height);
+  },
+
+  animate: function() {
+
+    requestAnimationFrame(this.animate);
+
+    this.clear();
+
+    this.context.fillStyle = 'gray';
+    this.context.fillRect(120, 0, 20, this.canvas.height);
+    this.context.fillRect(240, 0, 20, this.canvas.height);
+    this.context.fillRect(360, 0, 20, this.canvas.height);
+    this.context.fillRect(480, 0, 20, this.canvas.height);
+    this.context.fillRect(0, 335, this.canvas.width, 5);
+    this.context.fillRect(0, 360, this.canvas.width, 5);
+    this.context.fillStyle = 'green';
+
+    _.each(this.active, function(array){
+      _.each(array, function(target){
+        this.context.drawImage(this.sprites['marker'], (target.type * 120), target.top);
+      }, this)
+    }, this);
+
+    this.context.fillStyle = 'darkgray';
+
+    _.each(this.inactive, function(array){
+      _.each(array, function(target){
+        this.context.drawImage(this.sprites['marker'], (target.type * 120), target.top);
+      }, this)
+    }, this);
+
+    this.context.fillStyle = 'red';
+
+    _.each(this.missed, function(array){
+      _.each(array, function(target){
+        this.context.drawImage(this.sprites['marker'], (target.type * 120), target.top);
+      }, this)
+    }, this);
+
   },
 
   handleKey: function(event) {
