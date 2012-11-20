bdddf0cfd64be202faa964cacfbcab238901c456
conzett <conzett@gmail.com>
2012-11-19 23:29:39 -0500

    Add event bus for menus

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
+window.game = {};
+game.events = _.clone(Backbone.Events);
+
+game.events.on("start", function() {
+  console.log('insert song select view');
+});
+
+game.events.on("howto", function() {
+  console.log('insert how to play view');
+});
+
+game.events.on("highscores", function() {
+  console.log('insert highscores view');
+});
+
+game.events.on("credits", function() {
+  console.log('insert credits view');
+});
+
 var song = new SongView({
   model: songs.first()
 });
@@ -6,4 +25,4 @@ var menu = new MenuView();
 
 //$('body').append(song.el);
 
-$('body').append(menu.el);
+$('.container').append(menu.el);
