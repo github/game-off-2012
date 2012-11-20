238afa62fa2e21bbff255d6e267d9668d29676b7
Zoli Kahan <zolikahan@gmail.com>
2012-11-19 23:11:20 -0600

    added favicon

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3005);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
