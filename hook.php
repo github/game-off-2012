3fc63ffe1c1b1b518676ef04189e79fdfdf90922
crazy2be <crazy1be@gmail.com>
2012-11-22 20:18:18 -0500

    Updated hook

--- hook.php
+++ hook.php
@@ -3,4 +3,4 @@ $addr = $_SERVER['REMOTE_ADDR'];
 if ($addr !== '207.97.227.253' && $addr !== '50.57.128.197' && $addr !== '108.171.174.178') {
     exit;
 }
-system('git pull > ../log.txt');
+exec('git pull > ../log.txt');
