2d6a150e08a6acf10cf98ea4cb6f755264b48b66
crazy2be <crazy1be@gmail.com>
2012-11-23 12:30:37 -0500

    Updated hook some more, still trying to get it working.

--- hook.php
+++ hook.php
@@ -1,7 +1,7 @@
 <?php
 $addr = $_SERVER['REMOTE_ADDR'];
 if ($addr !== '207.97.227.253' && $addr !== '50.57.128.197' && $addr !== '108.171.174.178') {
-    echo "Access denied!";
+    exec("echo 'Access denied!' >> ../gitdefence.log");
     exit;
 }
-exec('git pull &> ../gitdefence.log');
+exec('git pull &>> ../gitdefence.log');
