840a603ad2cdec653713284d2247cb67866adba2
conzett <conzett@gmail.com>
2012-11-22 22:12:59 -0500

    Add list of songs to template

--- views/SongsView.js
+++ views/SongsView.js
@@ -1,4 +1,18 @@
 SongsView = ScreenView.extend({
     template: _.template($('#songs-template').html()),
-    className: 'songs'
+
+    className: 'songs',
+
+    render: function () {
+      ScreenView.prototype.render.apply(this);
+      this.collection.each(function(song){
+        this.renderSong(song);
+      }, this);
+    },
+
+    renderSong: function (song) {
+      this.$el.find('ul').append(_.template(
+        $("#song-list-item-template").html(), song.toJSON()
+      ));
+    }
 });
