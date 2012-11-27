487ad9158fdd97963743f7ef9631b8d1bf32ce8b
Kushagra Agarwal <kushagra666@gmail.com>
2012-11-27 16:56:37 +0530

    fixing touch controls

--- js/main.js
+++ js/main.js
@@ -193,7 +193,7 @@
     mit.flying_up = 1;
   }, false);
 
-  window.addEventListener('touchend', function(e) {
+  window.addEventListener('touchcancel', function(e) {
     if (!mit.start_btn_clicked)
       return;
 
