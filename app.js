a26a5647d429863540b7a31259927eb9774c096a
conzett <conzett@gmail.com>
2012-11-29 22:01:41 -0500

    Finish routing and menus
    Add routing between all views, fix bug with activeView destroy. Add splash screen with timeout. Add interaction with pause menu. Pause menu now activated with escape as well.
    
    Closes #60, #30, #29

--- app.js
+++ app.js
@@ -10,16 +10,42 @@ game.audio = {
 var Router = Backbone.Router.extend({
 
   routes: {
+    "" : "choctopus",
     "choose_song" : "choose_song",
     "song/:name"    : "song",
+    "howto"        : "howto",
+    "highscores"   : "highscores",
+    "credits"      : "credits",
+    "menu"         : "menu",
+    "choctopus"    : "choctopus"
   },
 
   choose_song: function() {
     game.chooseSong();
   },
 
+  howto: function() {
+    game.howTo();
+  },
+
+  highscores: function() {
+    game.highScores();
+  },
+
+  credits: function() {
+    game.credits();
+  },
+
+  menu: function() {
+    game.menu();
+  },
+
   song: function(name) {
     game.loadSong(songs.where({filename: name})[0]);
+  },
+
+  choctopus: function() {
+    game.choctopus();
   }
 
 });
@@ -34,9 +60,58 @@ game.refreshView = function(view) {
 
 game.chooseSong = function() {
   game.router.navigate("choose_song");
-  game.activeView.destroy();
+  if(game.activeView) game.activeView.destroy();
   game.activeView = new SongsView({
     collection: songs
+  });
+  game.refreshView();
+};
+
+game.howTo = function() {
+  game.router.navigate("howto");
+  if(game.activeView) game.activeView.destroy();
+  game.activeView = new HowtoView();
+  game.refreshView();
+};
+
+game.highScores = function() {
+  game.router.navigate("highscores");
+  if(game.activeView) game.activeView.destroy();
+  game.activeView = new HighScoreView({
+    collection : songs
+  });
+  game.refreshView();
+};
+
+game.credits = function() {
+  game.router.navigate("credits");
+  if(game.activeView) game.activeView.destroy();
+  game.activeView = new CreditsView({
+    collection: songs
+  });
+  game.refreshView();
+};
+
+game.choctopus = function() {
+  if(game.activeView) game.activeView.destroy();
+  game.activeView = new ChoctopusView();
+  game.refreshView();
+  window.setTimeout(game.menu, 2000);
+};
+
+game.menu = function() {
+  game.router.navigate("menu");
+  if(game.activeView) game.activeView.destroy();
+  game.activeView = new MenuView();
+  game.refreshView();
+  game.events.trigger('playSound', 'whip');
+};
+
+game.loadSong = function(song) {
+  game.router.navigate("song/" + song.get('filename'));
+  if(game.activeView) game.activeView.destroy();
+  game.activeView = new SongView({
+    model : song
   });
   game.refreshView();
 };
@@ -46,45 +121,27 @@ game.events.on("start", function() {
 });
 
 game.events.on("howto", function() {
-  game.activeView.destroy();
-  game.activeView = new HowtoView();
-  game.refreshView();
+  game.howTo();
 });
 
 game.events.on("highscores", function() {
-  game.activeView.destroy();
-  game.activeView = new HighScoreView({
-    collection : songs
-  });
-  game.refreshView();
+  game.highScores();
 });
 
 game.events.on("credits", function() {
-  game.activeView.destroy();
-  game.activeView = new CreditsView({
-    collection: songs
-  });
-  game.refreshView();
+  game.credits();
 });
 
 game.events.on("menu", function() {
-  if(game.activeView) game.activeView.destroy();
-  game.activeView = new MenuView();
-  game.refreshView();
-  game.events.trigger('playSound', 'whip');
+  game.menu();
 });
-
-game.loadSong = function(song) {
-  game.router.navigate("song/" + song.get('filename'));
-  game.activeView.destroy();
-  game.activeView = new SongView({
-    model : song
-  });
-  game.refreshView();
-}
 
 game.events.on("loadSong", function(song) {
   game.loadSong(song);
+});
+
+game.events.on("choctopus", function() {
+  game.choctopus();
 });
 
 game.events.on("playSound", function(sound) {
@@ -92,7 +149,5 @@ game.events.on("playSound", function(sound) {
     game.audio.effects[sound].play();
   }
 });
-
-game.events.trigger('menu');
 
 Backbone.history.start();
