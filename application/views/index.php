faaec23fea30df474467163380a677b6017e74f1
Thomas Jacobs <thmsjcbs@gmail.com>
2012-11-30 00:41:34 -0600

    Completely revamp styles

--- application/views/index.php
+++ application/views/index.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <title>Welcome to Fork It!</title>
-    <div id="levelReset" onclick="levelManager.resetLevel();">RESET</div>
     
     <!-- Resources -->
     <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script>
@@ -19,18 +18,29 @@
     <script type="text/javascript" src="js/scenes/gameBoard.js"></script>
     <script type="text/javascript" src="js/game.js"></script>
     <link href="css/common.css" rel="stylesheet" type="text/css">
-    <link href='http://fonts.googleapis.com/css?family=Montserrat+Alternates' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Scada' rel='stylesheet' type='text/css'>
 </head>
 <body>
-    <h1 id="gameTitle">Fork It!</h1>
+    <img id="gameTitle" src="images/title.png" />
 
     <div id="container">
-        <div id="cr-stage">
-        </div>
+        <div id="cr-stage"></div>
         <div id="levelTitle">Level 1: Moving blocks</div>
+        <div id="levelReset" onclick="levelManager.resetLevel();"><div class="largeText">RESET</div><div class="smallText">level</div></div>
+        <div class="clear"></div>
         <div id="levelText">To pass this level, you will need to use the SPACEBAR to grab and move blocks.</div>
+        <div id="controlText">
+            <span class="headerText">How To Play</span><br />
+            <span><b>Movement</b>: <b>W</b> (up) <b>A</b> (left) <b>S</b> (down) <b>U</b> (up)</span><br />
+            <span><b>Move Blocks</b>: Hold <b>SPACEBAR</b> to grab a block.  Use the movement keys to move the block</span><br />
+            <span><b>Remove Blocks</b>: <b>F</b> (There must be 3 touching blocks of the same color)</span><br />
+            <span><b>Take / Give Color</b>: <b>E</b> (Blocks with a white outline are <b>NOT</b> colorable)</span><br /><br />
+            <span class="headerText">Block Types</span><br />
+            <span><b>Colored Box</b>: These colored boxes can be pushed and pulled. You can also take and give colors to them. </span><br />
+            <span><b>Stationary Box</b>: These boxes are screwed to the floor.  They can not be pushed but they can still be removed by joining together other boxes of the same color!</span><br />
+            <span><b>Single Color Box</b>: Boxes with a white outline cannot have color taken or given to them.  They are locked to a single color.</span><br />
+        </div>
     </div>
-    <div id="backgroundContainer"></div>
 </body>
 </html>
