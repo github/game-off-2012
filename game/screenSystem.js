f41f494b6b6ef18453b70d191b6874881a883403
crazy2be <crazy1be@gmail.com>
2012-12-18 01:29:44 -0500

    Cleanup in ScreenSystem, really just trying to get a feel for how the entire event propogation system works.

--- game/screenSystem.js
+++ game/screenSystem.js
@@ -1,75 +1,62 @@
-﻿//System to switch between different things being active (like game, menu, etc)
-function ScreenSystem(canvasName) {
-    this.canvasName = canvasName;
+﻿ //System to switch between different things being active (like game, menu, etc)
 
-
-    var canvas = document.getElementById(canvasName.substring(1));
-
-    //Proper resize code is in mainGame.js (but not used)
+function ScreenSystem(canvas) {
+    //Proper resize code is in mainGame.js (well really inputHandler.js) (but not used)
     canvas.width = DFlag.width;
     canvas.height = DFlag.height;
-    
+
     var pen = canvas.getContext("2d");
 
-    this.screens = {};
-    var screens = this.screens;
-
+    var screens = {};
     var activeScreen = null;
 
-    window.reqAnim = (function () {
-        return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(function () {
-                callback(Date.now());
+    this.addScreen = function(name, screen) {
+        screens[name] = screen;
+    }
+
+    this.setActiveScreen = function(name) {
+        if (!screens[name]) return;
+        
+        activeScreen = screens[name];
+        activeScreen.screenSystem = this;
+
+        $(canvas).off();
+
+        if (screens[name].input) {
+            bindInput(screens[name].input);
+        }
+
+        if (activeScreen.gainFocus) activeScreen.gainFocus();
+    }
+
+    function bindInput(input) {
+        if (!input) return;
+        
+        var events = input.events;
+        for (var eventName in events) {
+            // Preserve this context
+            events[eventName] = events[eventName].bind(input);
+        }
+
+        for (var eventName in events) {
+            $(canvas).on(eventName, events[eventName]);
+        }
+    };
+    
+    var reqAnim = (function() {
+        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame || function(cb) {
+            window.setTimeout(function() {
+                cb(Date.now());
             }, 1000 / 60);
         };
     })();
-
-    this.addScreen = function(screenName, screen) {
-        screens[screenName] = screen;
-    }
-
-    this.setActiveScreen = function (screenName) {
-        if (screens[screenName]) {
-            activeScreen = screens[screenName];
-            activeScreen.screenSystem = this;
-
-            //Set up proper mouse events
-            $(this.canvasName).off();
-
-            if (screens[screenName].input) {
-                this.bindInput(screens[screenName].input);
-            }
-
-            if (activeScreen.gainFocus)
-                activeScreen.gainFocus();
-        }
-    }
-
-    //Already called in setActiveScreen
-    this.bindInput = function (input) {
-        if (input) {
-            var events = input.events;
-            for (var eventName in events) {
-                events[eventName] = events[eventName].bind(input);
-            }
-
-            for (var eventName in events) {
-                $(this.canvasName).on(eventName, events[eventName]);
-            }
-        }
-    };
     
     function tick(timestamp) {
         if (activeScreen && activeScreen.run) {
             activeScreen.run(timestamp);
             activeScreen.draw(pen);
         }
-        window.reqAnim(tick.bind(this));
+        reqAnim(tick.bind(this));
     }
-    window.reqAnim(tick.bind(this));
+    reqAnim(tick.bind(this));
 }
