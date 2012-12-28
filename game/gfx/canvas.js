ac222e6f518ba20dceebb51eb9e0abdd98903854
crazy2be <crazy1be@gmail.com>
2012-12-28 12:47:02 -0700

    Initial attempt at canvas class.

--- game/gfx/canvas.js
+++ game/gfx/canvas.js
@@ -1,3 +1,35 @@
 function Canvas() {
+    var element = document.createElement('canvas');
+    var c = element.getContext('2d');
+    var pos;
     
+    this.resize = function (rect) {
+        pos = rect.origin();
+        element.width = rect.w;
+        element.height = rect.h;
+        return this;
+    }
+    
+    this.drawTo = function (otherCanvas) {
+        otherCanvas.drawImage(c, pos.x, pos.y);
+    }
+    
+    // Required for drawTo to work properly on the canvas
+    // class in addition to "raw" canvas contexts.
+    this.drawImage = function (img, x, y) {
+        c.drawImage(img, x, y);
+    }
+    
+    this.strokeRect = function (rect, color, width) {
+        c.lineWidth = width;
+        c.strokeStyle = color && color.str ? color.str() : color;
+        c.strokeRect(rect.x, rect.y, rect.w, rect.h);
+        return this;
+    }
+    
+    this.fillRect = function (rect, color) {
+        c.fillStyle = color && color.str ? color.str() : color;
+        c.fillRect(rect.x, rect.y, rect.w, rect.h);
+        return this;
+    }
 }
