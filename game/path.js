3e7db8c89bc1f446736549c37708291e596f4e27
crazy2be <crazy1be@gmail.com>
2013-05-08 21:15:40 -0400

    Cache line drawing.

--- game/path.js
+++ game/path.js
@@ -1,7 +1,7 @@
 function Path_End(x, y, w, h) {
     this.tpos = new Rect(x, y, w, h);
     this.base = new BaseObj(this, 2);
-    
+
     this.draw = function (pen) {
         var p = this.tpos;
         pen.fillStyle = "grey";
@@ -13,7 +13,7 @@ function Path_End(x, y, w, h) {
 function Path_Start(x, y, w, h) {
     this.tpos = new Rect(x, y, w, h);
     this.base = new BaseObj(this, 2);
-    
+
     this.update = function (dt) {
         if (this.nextPath && !this.pathLine) {
             var eng = getEng(this);
@@ -22,7 +22,7 @@ function Path_Start(x, y, w, h) {
             eng.base.addChild(this.pathLine);
         }
     };
-    
+
     this.draw = function (pen) {
         var p = this.tpos;
         pen.fillStyle = "yellow";
@@ -34,27 +34,37 @@ function Path_Start(x, y, w, h) {
 
 function Path_Line(pathBase) {
     this.path = pathBase;
-    
+
     //Our shape is a lie! (its off, not that it really matters)
     this.tpos = pathBase.tpos;
     this.base = new BaseObj(this, 3);
-    
-    this.draw = function (pen) {
-        if (pathBase.nextPath) {
-            var t = pathBase.nextPath.tpos.center();
-            var direction = new Vector(t.x, t.y);
-            direction.sub(pathBase.tpos.center());
 
-            var start = pathBase.tpos.center();
+    this.redraw = function (canvas) {
+        if (!pathBase.nextPath) return;
 
-            var end = new Vector(start.x, start.y);
-            direction.norm().mult(pathBase.tpos.w);
-            end.add(direction);
+        var p = this.tpos;
+        // tpos x/y give us the origin of our arrow, and w-h give us the
+        // size of our arrow, However, I'm too lazy to figure out what
+        // direction the arrow is going in and resize/translate the canvas
+        // based on that, so instead I just make it twice as large as
+        // necessary.
+        canvas.resize(new Rect(p.x-p.w/2, p.y-p.h/2, p.w*2, p.h*2));
+        var pen = canvas.ctx();
+        pen.translate(p.w/2-p.x, p.h/2-p.y);
 
-            pen.strokeStyle = "blue";
-            pen.lineWidth = 2;
-            ink.arrow(start.x, start.y, end.x, end.y, pen);
-        }
+        var t = pathBase.nextPath.tpos.center();
+        var direction = new Vector(t.x, t.y);
+        direction.sub(pathBase.tpos.center());
+
+        var start = pathBase.tpos.center();
+
+        var end = new Vector(start.x, start.y);
+        direction.norm().mult(pathBase.tpos.w);
+        end.add(direction);
+
+        pen.strokeStyle = "blue";
+        pen.lineWidth = 2;
+        ink.arrow(start.x, start.y, end.x, end.y, pen);
     };
 }
 
