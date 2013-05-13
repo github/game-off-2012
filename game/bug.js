78dfbb00069acd7355431e2247f58ebcfc0899f8
crazy2be <crazy1be@gmail.com>
2013-05-12 21:41:11 -0400

    Cache bug drawing, working towards #99.

--- game/bug.js
+++ game/bug.js
@@ -1,4 +1,5 @@
 function Bug(startPath) {
+    var self = this;
     var r = 8;
     var cen = (function() {
         var p = startPath.tpos;
@@ -8,16 +9,16 @@ function Bug(startPath) {
         return cen;
     }());
 
-    this.attr = {};
-    this.setBaseAttrs = function () {
+    self.attr = {};
+    self.setBaseAttrs = function () {
         //Lol, prevCur...
-        var prevCurHp = this.attr.hp || this.attr.currentHp;
+        var prevCurHp = self.attr.hp || self.attr.currentHp;
         if(!prevCurHp)
             prevCurHp = 0;
-        this.attr = {
+        self.attr = {
             // For balancing these are now 0, we get everything from our alleles
             // (except speed, as we have to move, and value).
-            // In the future tower will be like this.
+            // In the future tower will be like self.
             range:          0,
             damage:         0,
             hp:             0,
@@ -29,127 +30,157 @@ function Bug(startPath) {
             kills:          0,
             value:          5,
         };
-        this.attr.attackTypes = [];
+        self.attr.attackTypes = [];
     }
-    this.setBaseAttrs();
+    self.setBaseAttrs();
 
-    this.tpos = new Rect(cen.x - r, cen.y - r, r * 2, r * 2);
-    this.base = new BaseObj(this, 11);
-    var velocity = new Vector(1, 0).mag(this.attr.speed);
+    self.tpos = new Rect(cen.x - r, cen.y - r, r * 2, r * 2);
+    self.base = new BaseObj(self, 11);
+    var velocity = new Vector(1, 0).mag(self.attr.speed);
 
     //Will be replaced
-    this.color = "trasparent";
-    this.borderColor =  "red";
+    self.color = "trasparent";
+    self.borderColor =  "red";
 
-    this.lineWidth = 1;
-    this.radius = r;
-
-
-    this.genes = new Genes();
-    this.base.addChild(this.genes);
+    self.lineWidth = 1;
+    self.radius = r;
 
 
+    self.genes = new Genes();
+    self.base.addChild(self.genes);
 
-    this.base.addChild(new AttackCycle());
 
-    this.base.addChild(new Selectable());
 
-    this.curPath = startPath;
+    self.base.addChild(new AttackCycle());
 
-    this.added = function() {
-        var game = getGame(this);
+    self.base.addChild(new Selectable());
 
-        this.bugRelPathPos = Math.floor(Math.random() * game.tileSize) +1;
-        this.delay = this.bugRelPathPos + 1;
+    self.curPath = startPath;
 
-        this.constantOne = 1;
-        this.base.addChild(new UpdateTicker(this, "constantOne", "regenTick"));
+    self.added = function() {
+        var game = getGame(self);
+
+        self.bugRelPathPos = Math.floor(Math.random() * game.tileSize) +1;
+        self.delay = self.bugRelPathPos + 1;
+
+        self.constantOne = 1;
+        self.base.addChild(new UpdateTicker(self, "constantOne", "regenTick"));
     };
-    
-    this.regenTick = function() {
-        if (this.attr.hpRegen > 0) {
-            this.attr.currentHp += this.attr.hpRegen;
+
+    self.regenTick = function() {
+        if (self.attr.hpRegen > 0) {
+            self.attr.currentHp += self.attr.hpRegen;
         }
-        if (this.attr.currentHp > this.attr.hp) {
-            this.attr.currentHp = this.attr.hp;
+        if (self.attr.currentHp > self.attr.hp) {
+            self.attr.currentHp = self.attr.hp;
         }
     }
-    
+
     function move(pos, vec, dt) {
         pos.x += vec.x * dt;
         pos.y += vec.y * dt;
     }
 
-    this.update = function(dt) {
-        move(this.tpos, velocity, dt);
+    var previousHp = -1;
+    var canvasDirty = true;
+    self.update = function(dt) {
+        move(self.tpos, velocity, dt);
 
-        var cur = this.curPath;
-        var next = this.curPath.nextPath;
+        var cur = self.curPath;
+        var next = self.curPath.nextPath;
 
         //Move towards the next rectangle.
-        var vecToCurrent = minVecBetweenRects(this.tpos, cur.tpos);        
-        var vecToNext = minVecFullOverlapRects(this.tpos, next.tpos);
+        var vecToCurrent = minVecBetweenRects(self.tpos, cur.tpos);
+        var vecToNext = minVecFullOverlapRects(self.tpos, next.tpos);
 
         var overshot = vecToCurrent.magSq() > 0 && vecToNext.magSq() > 0;
 
         if (overshot) {
-            move(this.tpos, velocity, -dt);
+            move(self.tpos, velocity, -dt);
         }
 
-        if (this.delay > this.bugRelPathPos) {
-            velocity = vecToNext.clone();          
-            this.delay = 0;
+        if (self.delay > self.bugRelPathPos) {
+            velocity = vecToNext.clone();
+            self.delay = 0;
         }
 
         //Once we reach our destination.
-        if (vecToNext.magSq() == 0 || overshot) {            
-            this.delay += 50*dt;
-            if (this.delay > this.bugRelPathPos || overshot) {
-                this.curPath = next;
-                this.delay = this.bugRelPathPos + 1;
+        if (vecToNext.magSq() == 0 || overshot) {
+            self.delay += 50*dt;
+            if (self.delay > self.bugRelPathPos || overshot) {
+                self.curPath = next;
+                self.delay = self.bugRelPathPos + 1;
             }
 
             if (next instanceof Path_End) {
-                this.destroyAtBase();
+                self.destroyAtBase();
             }
         }
-         
-        velocity.mag(this.attr.speed);
-        
 
-        this.color = getInnerColorFromAttrs(this.attr);
-        this.borderColor = getOuterColorFromAttrs(this.attr);
+        velocity.mag(self.attr.speed);
+
+        // We only invalidate when our HP changes, since that covers most
+        // cases, and other things don't really change much for bugs.'
+        if (self.attr.currentHp === previousHp) return;
+
+        canvasDirty = true;
+        self.color = getInnerColorFromAttrs(self.attr);
+        self.borderColor = getOuterColorFromAttrs(self.attr);
+        previousHp = self.attr.hp;
     };
 
-    this.destroyAtBase = function() {
-        var game = this.base.rootNode.game;
-        var eng = this.base.rootNode;
-        
+    self.destroyAtBase = function() {
+        var game = self.base.rootNode.game;
+        var eng = self.base.rootNode;
+
         game.health -= 5;
 
         if (game.health <= 0 && !eng.base.allLengths.GameOver) {
             eng.base.addChild(new GameOver());
         }
 
-        this.base.destroySelf();
+        self.base.destroySelf();
     };
 
-    this.draw = function(pen) {
-        var pos = this.tpos;
+    var canvas = new Canvas();
+    self.draw = function(pen) {
+        if (canvasDirty) {
+            redraw(canvas);
+            canvasDirty = false;
+        }
+        canvas.moveTo(new Vector(self.tpos.x - self.attr.range, self.tpos.y - self.attr.range));
+        canvas.drawTo(pen);
+    }
+
+    function redraw(canvas) {
+        var range = self.attr.range;
+        canvas.resize(new Rect(0, 0, range*2, range*2));
+
+        var pen = canvas.ctx();
+        pen.save();
+        pen.translate(
+            range - self.tpos.x - self.tpos.w/2,
+            range - self.tpos.y - self.tpos.h/2);
+        actualRedraw(pen);
+        pen.restore();
+    }
+
+    function actualRedraw(pen) {
+        var pos = self.tpos;
         var cen = pos.center();
 
-        var hpPercent = this.attr.currentHp / this.attr.hp;
+        var hpPercent = self.attr.currentHp / self.attr.hp;
         var hue = hpPercent * 135;
 
-        DRAW.arc(pen, cen, this.radius + this.lineWidth,
-            0, Math.PI * 2 * hpPercent,
-            new HSLColor().h(hue).s(50).l(50).a(0).str(), 3,
-            new HSLColor().h(hue).s(90).l(60).a(1).str());
+        DRAW.arc(pen, cen, self.radius + self.lineWidth,
+                 0, Math.PI * 2 * hpPercent,
+                 hsla(hue, 50, 50, 0).str(), 3,
+                 hsla(hue, 90, 60, 1).str());
 
-        DRAW.circle(pen, cen, this.radius,
-                    this.color, 1, this.borderColor);
+        DRAW.circle(pen, cen, self.radius,
+                    self.color, 1, self.borderColor);
 
-        DRAW.circle(pen, cen, this.attr.range,
-                    setAlpha(this.color, 0.13));
+        DRAW.circle(pen, cen, self.attr.range,
+                    setAlpha(self.color, 0.13));
     }
 }
--- game/gfx/canvas.js
+++ game/gfx/canvas.js
@@ -2,19 +2,24 @@ function Canvas() {
     var element = document.createElement('canvas');
     var c = element.getContext('2d');
     var pos = new Vector(0, 0);
-    
+
     this.resize = function (rect) {
         pos = rect.origin();
         element.width = rect.w;
         element.height = rect.h;
         return this;
     }
-    
+
+    this.moveTo = function (newOrigin) {
+        pos = newOrigin;
+        return this;
+    }
+
     this.drawTo = function (otherCanvas) {
     // Without this, integer values end up doing sub-pixel
     // rendering. Who knows what the W3C was thinking?
     // See http://diveintohtml5.info/canvas.html#paths
-    
+
     // I can't seem to get this translate code to work, so
     // I'm just manually translating for now. Good luck if
     // you can get it to work!
@@ -26,13 +31,13 @@ function Canvas() {
         }
         otherCanvas.drawImage(element, pos.x, pos.y);
     }
-    
+
     // Required for drawTo to work properly on the canvas
     // class in addition to "raw" canvas contexts.
     this.drawImage = function (img, x, y) {
         c.drawImage(img, x, y);
     }
-    
+
     this.stroke = function (path, color, width) {
         c.beginPath();
         path.apply(c, "stroke");
@@ -40,14 +45,14 @@ function Canvas() {
         c.strokeStyle = color && color.str ? color.str() : color;
         c.stroke();
     }
-    
+
     this.fill = function (path, color) {
         c.beginPath();
         path.apply(c, "fill");
         c.fillStyle = color && color.str ? color.str() : color;
         c.fill();
     }
-    
+
     // Try not to use this!
     this.ctx = function () {
         return c;
