18f5d10dbe0786d07b474c0de2cc2e3e8efce50b
bergren2 <dan@heymrbass.com>
2014-07-13 12:23:17 -0500

    update GA id

--- application/views/index.php
+++ application/views/index.php
@@ -58,7 +58,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-48986974-1', 'forkit.herokuapp.com');
+  ga('create', 'UA-45209003-2', 'forkit.herokuapp.com');
   ga('send', 'pageview');
 
 </script>
